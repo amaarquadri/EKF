d482af10f26589c0fd2308566579243c200f07a5
Amaar Quadri <amaarquadri@gmail.com>
2022-03-17 01:44:03 -0400

    decrease pitch gains

--- bmb_world_model/include/bmb_world_model/Constants.h
+++ bmb_world_model/include/bmb_world_model/Constants.h
@@ -68,7 +68,7 @@ static constexpr size_t p =
 // controller constants
 static constexpr ControllerGains THROTTLE_GAIN{1, 0, 0, 1};
 static constexpr ControllerGains ROLL_GAIN{1, 0, 0};
-static constexpr ControllerGains PITCH_GAIN{1, 0, 0, 1};
+static constexpr ControllerGains PITCH_GAIN{0.01, 0, 0, 0.01};
 static constexpr ControllerGains ELEVATOR_GAIN{1, 1, 1, 1};
 static constexpr double PROPELLER_K_P = 1;
 static constexpr ControllerGains AILERON_GAIN{1, 1, 1, 1};
