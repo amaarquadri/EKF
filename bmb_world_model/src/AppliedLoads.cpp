6e3c3baac9c4be6c28300c83d085ce4a25e34b86
Amaar Quadri <amaarquadri@gmail.com>
2022-03-17 17:25:23 -0400

    enable aileron roll torques

--- bmb_world_model/src/AppliedLoads.cpp
+++ bmb_world_model/src/AppliedLoads.cpp
@@ -12,7 +12,7 @@
 
 // aerodynamic slope constants
 static constexpr Wrench<double> BODY_M_WRENCH{};
-static constexpr Wrench<double> AILERON_M_WRENCH{};
+static constexpr Wrench<double> AILERON_M_WRENCH{0, 0, 0, 0.0547822725108043};
 static constexpr Wrench<double> ELEVATOR_M_WRENCH{
     0, 0, 0, 0, 0.0842508724650606, 0};
 static constexpr Wrench<double> RUDDER_M_WRENCH{};
