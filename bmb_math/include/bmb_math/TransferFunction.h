ce4cab5086bc3ef27febb79aa1cb63e8de4f3a8b
corwinmacmillan <cgjmacmi@edu.uwaterloo.ca>
2022-03-18 01:14:33 -0400

    got rid of discrete parameter so that discrete Transfer functions can be made. just make sure you dont use step or next_output with a continuous transfer function

--- bmb_math/include/bmb_math/TransferFunction.h
+++ bmb_math/include/bmb_math/TransferFunction.h
@@ -12,7 +12,6 @@ class TransferFunction : public RationalFunction<T, n, m> {
   static_assert(m >= n, "Transfer function is not proper!");
   static_assert(n != 0 && m != 0,
                 "Numerator and denominator must have at least 1 element!");
-  bool discrete = false;
   Vector<T, m>
       past_inputs;  // only use the n oldest of these values at each time step
   Vector<T, m - 1> past_outputs;
@@ -73,7 +72,6 @@ class TransferFunction : public RationalFunction<T, n, m> {
 
     TransferFunction<T, p, q> discretized = this->_of_(trapezoidal);
     discretized.normalize();
-    discretized.discrete = true;
     return discretized;
   }
 
@@ -81,7 +79,6 @@ class TransferFunction : public RationalFunction<T, n, m> {
    * The output should not be used in algebraic loops if n == m
    */
   T next_output(const T& input) {
-    assert(this->discrete);
     T next_output = static_cast<T>(0);
     past_inputs = past_inputs.pushFrontPopBack(input);
     for (size_t i = 0; i < m - 1; i++)
@@ -95,7 +92,6 @@ class TransferFunction : public RationalFunction<T, n, m> {
 
   template <size_t output_size>
   Vector<T, output_size> step() {
-    assert(this->discrete);
     Vector<T, output_size> step_response;
     for (size_t i = 0; i < output_size; i++)
       step_response[i] = next_output(static_cast<T>(1));
