2f18f3db5049039b7aecb02590538bb8a534a730
Amaar Quadri <amaarquadri@gmail.com>
2022-03-17 21:09:43 -0400

    replace pitch_smoother time constant from heuristic to tuned value

--- bmb_controllers/src/StateCommandSmoother.cpp
+++ bmb_controllers/src/StateCommandSmoother.cpp
@@ -3,24 +3,11 @@
 #include <bmb_msgs/StateCommand.h>
 #include <bmb_utilities/MathUtils.h>
 
-/**
- * Based on max tail velocity of 0.3m/s and a 1.3m distance from the rotation
- * axis to the tail.
- */
-static constexpr double MAX_PITCH_RATE = 0.3 / 1.3;  // rad/s
-
 static const TransferFunction<double, 1, 2> SPEED_SMOOTHER{1, 0.1, 1};
 
 static const TransferFunction<double, 1, 2> ROLL_SMOOTHER{1, 2, 1};
 
-/**
- * Based on a most aggressive command of changing from -7 degrees to +8 degrees.
- * This is a step input of 15 degrees.
- * Setting the time constant to 15 degrees / MAX_PITCH_RATE ensures that in this
- * worst case scenario the pitch rate will equal the MAX_PITCH_RATE.
- */
-static const TransferFunction<double, 1, 2> PITCH_SMOOTHER{
-    1, bmb_utilities::deg2rad(15) / MAX_PITCH_RATE, 1};
+static const TransferFunction<double, 1, 2> PITCH_SMOOTHER{1, 2, 1};
 
 StateCommandSmoother::StateCommandSmoother(const double& update_frequency) {
   const double dt = 1 / update_frequency;
